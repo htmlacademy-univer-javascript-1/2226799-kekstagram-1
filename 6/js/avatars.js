ecfb34282af23dca1bd642e1ecb86f5af50ee376
Keks <keks@htmlacademy.ru>
2022-12-08 21:31:41 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/avatars.js
@@ -0,0 +1,28 @@
+import {photos} from './data.js';
+import {openFullscreenPhoto} from './fullscreenPicture.js';
+
+const picturesElementsContainer = document.querySelector('.pictures');
+const picturesListFragment = document.createDocumentFragment();
+const photoTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const appendPicture = (picture) => {
+  const {url, likes, comments} = picture;
+  const pictureElement = photoTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.addEventListener('click', () => {
+    openFullscreenPhoto(picture);
+  });
+  picturesListFragment.appendChild(pictureElement);
+};
+
+const renderPictures = () => {
+  photos().forEach(appendPicture);
+  picturesElementsContainer.appendChild(picturesListFragment);
+};
+
+export {renderPictures};
